f7aecf49fdb955883b824f9e5e04fb8ac24768fb
Sherzodbek <sherzoduralov285@gmail.com>
2024-10-04 17:48:28 +0900

    feat: MUI stardart integration

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,10 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import './css/index.css';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
