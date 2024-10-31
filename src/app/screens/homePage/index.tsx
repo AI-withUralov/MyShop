f4396b60ac32001a0bbf465cc6e84929a9488402
Sherzodbek <sherzoduralov285@gmail.com>
2024-10-31 22:43:32 +0900

    feat: develop silice and selector usage

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -7,20 +7,38 @@ import ActiveUsers from "./AcitveUsers";
 import Events from "./Events";
 import '../../../css/home.css'
 
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setPopularDishes } from "./slice";
+import { retrievePopularDishes } from "./selector";
+import { Product } from "../../../lib/types/product";
+import { useEffect } from "react";
 
+/** REDUX SLICE & SELECTOR */
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+});
 
+const popularDishesRetriever = createSelector(
+  retrievePopularDishes,
+  (popularDishes) => ({ popularDishes })
+);
 
- export default function HomePage() {
-    return (
-    <div className={"homepage"}>
-      <Statistics/>
-      <PopularDishes/>
-      <NewDishes/>
-      <Advertisement/>
-      <ActiveUsers/>
-      <Events/>
-    </div>)
+export default function HomePage() {
+  const { setPopularDishes } = actionDispatch(useDispatch());
+  const { popularDishes } = useSelector(popularDishesRetriever);
 
-    
-  }
-  
+  useEffect(() => {}, []);
+
+  return (
+    <div className="homepage">
+      <Statistics />
+      <PopularDishes />
+      <NewDishes />
+      <Advertisement />
+      <ActiveUsers />
+      <Events />
+    </div>
+  );
+}
